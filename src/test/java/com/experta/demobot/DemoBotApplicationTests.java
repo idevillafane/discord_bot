0c1e4c17623b18c266fb8aeea28e3eed716b37fb
Ignacio de Villafañe <idevillafane@gmail.com>
2022-11-14 16:07:16 -0300

    Sincronizo repos

---
+++ src/test/java/com/experta/demobot/DemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experta.demobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
