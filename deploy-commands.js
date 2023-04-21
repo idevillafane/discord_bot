fd306a285bf41d6b37a8e64896d80492f8e20e77
Ignacio de Villafañe <idevillafane@gmail.com>
2023-04-21 11:41:38 -0300

    trello-mode-api

---
+++ deploy-commands.js
@@ -0,0 +1,44 @@
+import fs from "fs"
+import path from "path"
+import { REST, Routes } from "discord.js"
+import dotenv from 'dotenv'
+dotenv.config()
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const foldersPath = path.join(new URL('.', import.meta.url).pathname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const { default: command } = await import(filePath);
+		commands.push(command.data.toJSON());
+	}
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(process.env.DISCORD_APPLICATION_ID, process.env.DISCORD_GUILD_ID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		process.exit();
+	}
+	catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
