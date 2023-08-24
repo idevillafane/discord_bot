6e6472835923d788b638132c17a62f878338d132
Ignacio de Villafañe <nacho@MacBook-Air-de-Ignacio.local>
2023-08-24 17:32:46 -0300

    cluster redis configurado

--- redis_db/redis.js
+++ redis_db/redis.js
@@ -1,6 +1,8 @@
 import { createClient } from 'redis'
 
-const client = createClient();
+const client = createClient({
+  url: 'cluster-test.art.com:6379'
+});
 
 client.on('error', err => console.log('Redis Client Error', err));
 
