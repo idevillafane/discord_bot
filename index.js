4ad205cd872858aa75a9f8a6408b59d70132043f
Ignacio de Villafañe <idevillafane@gmail.com>
2024-03-13 18:24:43 -0300

    fixed

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import { Client, Collection, Events, GatewayIntentBits } from 'discord.js'
 import dotenv from 'dotenv'
 import path from 'path'
 import fs from 'fs'
 import { discordReply } from './utils/discordReply.js'
-=======
-import fs from "fs"
-import path from "path"
-import { Client, Collection, Events, GatewayIntentBits } from "discord.js"
-import dotenv from 'dotenv'
->>>>>>> fd306a2 (trello-mode-api)
 dotenv.config()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -22,7 +15,6 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-<<<<<<< HEAD
 client.login(process.env.DISCORD_TOKEN);
 
 
@@ -75,54 +67,3 @@ client.on(Events.InteractionCreate, async interaction => {
         return;
     }
 });
-=======
-// Log in to Discord with your client's token
-client.login(process.env.DISCORD_TOKEN);
-
-
-const foldersPath = path.join(new URL('.', import.meta.url).pathname, 'commands');;
-
-
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-		const { default: command } = await import(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		}
-		else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
-
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	}
-	catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-		else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
->>>>>>> fd306a2 (trello-mode-api)
